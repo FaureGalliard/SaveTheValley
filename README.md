c27844f4a3b5655290f0949e508a926ace365db4
Ángel Gabriel Crispín Valdivia <angelcrispinvaldivia@gmail.com>
2025-07-29 09:39:27 -0500

    Eleccion final de la version de sfml,correccion de uso de compilador, mejoras al cmakepresets y compilacion

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 # Instrucciones
+sfml 2.6.2 
+
 ## Compilar
-Elige modo el modo que quieras:
-### debug
-cmake --preset debug
-cmake --build --preset debug
-### release
-cmake --preset release
-cmake --build --preset release
-## ----------------
+cmake --preset default
+cmake --build --preset default
+
 ### Linux:
- Asegurate de tener descargado sfml 3.0.0 y ejecuta comandos de arriba
+ Asegurate de tener descargado sfml 2.6.2
+ https://www.sfml-dev.org/download/sfml/2.6.2/
+ y ejecuta comandos de arriba
 ### Windows:
- Compila con los comandos de arriba y despues copia los .dll de lib/SFML/bin con esta forma sfml-xx-3.dll
+ Debes tener este compilador para usar los binarios ya compilados de sfml: 
+ https://www.sfml-dev.org/files/SFML-2.6.2-windows-gcc-13.1.0-mingw-64-bit.zip
+ Compila con los comandos de arriba y despues copia los .dll de lib/SFML/bin con esta forma sfml-xx-2.dll
--- src/core/main.cpp
+++ src/core/main.cpp
@@ -1,21 +1,46 @@
+//main.cpp
 #include <SFML/Graphics.hpp>
 #include <nlohmann/json.hpp>
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode({200, 200}), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+int main() {
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Mi Juego con UI");
+    window.setFramerateLimit(60);
 
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
+    // Botón (rectángulo)
+    sf::RectangleShape boton(sf::Vector2f(200.f, 60.f));
+    boton.setPosition(300.f, 250.f);
+    boton.setFillColor(sf::Color::Blue);
+
+    // Texto del botón
+    sf::Font font;
+    if (!font.loadFromFile("assets/fonts/NotoSans-Regular.ttf")) return -1;
+
+    sf::Text texto("¡Botón presionado!", font, 24);
+    texto.setFillColor(sf::Color::White);
+    texto.setPosition(340.f, 265.f);  // Ajusta manualmente
+
+    // Bucle principal
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
                 window.close();
+
+            // Click en el botón
+            if (event.type == sf::Event::MouseButtonPressed &&
+                event.mouseButton.button == sf::Mouse::Left) {
+                sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+                if (boton.getGlobalBounds().contains((float)mousePos.x, (float)mousePos.y)) {
+                    // Acción del botón
+                    printf("¡Botón presionado!\n");
+                }
+            }
         }
 
-        window.clear();
-        window.draw(shape);
+        window.clear(sf::Color::Black);
+        window.draw(boton);
+        window.draw(texto);
         window.display();
     }
+
+    return 0;
 }
